a50db568fe5e4e71a11791bfb6148cebc689f847
Heller <austin.heller@hotmail.ca>
2025-06-12 16:29:17 -0400

    UI stuff and event stuff

--- Scripts/event.gd
+++ Scripts/event.gd
@@ -6,3 +6,4 @@ var weight:int
 func _init(event_id:int, event_weight:int = 1):
 	id = event_id
 	weight = event_weight
+	
--- Scripts/event_manager.gd
+++ Scripts/event_manager.gd
@@ -10,6 +10,21 @@ var event_queue:Array[Event] = []
 func _ready() -> void:
 	events = preload("res://data/events.csv")
 	print(events.records)
+
+func next_event() -> int:
+	var weighting_list:Array[int] = []
+	
+	for event in events:
+		for n in event.weight:
+			weighting_list.append(event.id)
+	
+	var chosen_event = weighting_list.pick_random()
+	
+	event_queue.remove_at(event_queue.find(chosen_event))
+	
+	return chosen_event.id
+
+
 	
 func add_to_queue(event_id:int, event_weight:int = 1):
 	event_queue.append(Event.new(event_id, event_weight))
