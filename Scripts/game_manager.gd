d1d5084d250981011038a10dd2c0caf097f7f31c
Heller <austin.heller@hotmail.ca>
2025-06-18 15:51:55 -0400

    minor additions/adjustments

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -26,11 +26,12 @@ const STARTING_HAPPINESS:float = 10.0
 const STARTING_TAX_RATE:float = 0.10
 
 @onready var gold_text:RichTextLabel = get_node("/root/Main/UI/TopBar/GoldText")
-@onready var happiness_icon:AnimatedSprite2D = get_node("/root/Main/UI/TopBar/HappinessIcon")
+@onready var happiness_tooltip:Control= get_node("/root/Main/UI/TopBar/HappinessTooltip")
+@onready var happiness_icon:AnimatedSprite2D = get_node("/root/Main/UI/TopBar/HappinessTooltip/HappinessIcon")
 
 var player_faction:Faction
 
-var all_zones = {}
+var all_zones: Dictionary[String, Zone] = {}
 
 func _ready() -> void:
 	player_faction = Faction.new(PLAYER_NAME, self, STARTING_GOLD, STARTING_HAPPINESS, STARTING_TAX_RATE)
@@ -44,7 +45,6 @@ func change_player_stat(stat_type:int, change_value):
 	player_faction.change_stat(stat_type, change_value)
 	ui_update(stat_type)
 
-
 func set_player_stat (stat_type:int, set_value):
 	player_faction.set_stat(stat_type, set_value)
 	ui_update(stat_type)
@@ -57,7 +57,7 @@ func ui_update(stat_type):
 
 func update_happiness():
 	var cur_happiness = player_faction.get_stat(stats.HAPPINESS)
-	
+	happiness_tooltip.tooltip_text = "Happiness: "+str(cur_happiness)
 	if(cur_happiness > 15):
 		happiness_icon.frame = 0
 	elif(cur_happiness <= 15 and cur_happiness > 5):
@@ -68,7 +68,6 @@ func update_happiness():
 		happiness_icon.frame = 3
 	elif(cur_happiness < -15):
 		happiness_icon.frame = 4
-	
 
 func update_gold():
 	gold_text.text = str(player_faction.gold)
